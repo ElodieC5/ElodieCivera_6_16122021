4486f4c845e638126a09b4867aa17b6f9e6dffae
Elodie Civera <elodie.civera5@gmail.com>
2022-03-15 19:00:02 +0100

    work on modal contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,14 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+
+    
+
+    const whichPhotographer = document.getElementsByClassName("photographer-contact");
+    const id = document.querySelector(".photograph-header article > h2");
+    whichPhotographer[0].innerHTML = id.innerHTML;
+    
+    
 }
 
 function closeModal() {
