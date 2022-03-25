b67bcad14ef4df70c0e754d54eeba211aa6752cd
Elodie Civera <elodie.civera5@gmail.com>
2022-03-25 09:52:00 +0100

    function total likes

--- scripts/factories/mediasFactory.js
+++ scripts/factories/mediasFactory.js
@@ -1,61 +1,33 @@
-
 //      Factory to build media template
 //      Create media cards inside the selected photographer page
 //     -------------------------------------------------------------------------------------------
 
-
 // constructor pattern for the media cards
 class MediaCard {
+	constructor(selectedPhotographer, mediaSample) {
+		this.selectedPhotographer = selectedPhotographer;
+		this.mediaSample = mediaSample;
+	}
 
-    constructor(selectedPhotographer, mediaSample) {
-        this.selectedPhotographer = selectedPhotographer;
-        this.mediaSample = mediaSample;
-    }
-    
-    createMediaCard() {
-    let namePortrait = this.selectedPhotographer.name;
-    namePortrait = namePortrait.split(" ");
+	createMediaCard() {
+		let namePortrait = this.selectedPhotographer.name;
+		namePortrait = namePortrait.split(" ");
 
-    const mediaImg = `<img alt="${this.mediaSample.title}" src="assets/photographers/${namePortrait[0]}/${this.mediaSample.image}" />`;
-    const mediaVid = `<video alt="${this.mediaSample.title}" src="assets/photographers/${namePortrait[0]}/${this.mediaSample.video}"></video>`;
+		const mediaImg = `<img alt="${this.mediaSample.title}" src="assets/photographers/${namePortrait[0]}/${this.mediaSample.image}" />`;
+		const mediaVid = `<video alt="${this.mediaSample.title}" src="assets/photographers/${namePortrait[0]}/${this.mediaSample.video}"></video>`;
 
-    return `
+		return `
         <div class="media-thumbnail">
-            ${this.mediaSample.image ? mediaImg : mediaVid}
-            <h3>${this.mediaSample.title}</h3>
-            <div class="likes-btn" aria-label="likes">
-                ${this.mediaSample.likes}
-                <img class="likes-btn" src="assets/icons/heart.svg" />
-            </div>
-        </div>
-        `;
-    };
+        ${this.mediaSample.image ? mediaImg : mediaVid}
+    <h3>${this.mediaSample.title}</h3>
+    <div class="likes-btn" aria-label="likes">
+    ${this.mediaSample.likes}
+    <div class="container-image">
+    <img class="likes-btn" src="assets/icons/heart.svg" />
+    </div>
+    </div>
+    </div>
+    `;
+	}
+
 }
- // create media cards using literal template
-
-
-// this.wrapper.innerHTML = mediaCard;
-// this.handleLikesButton();
-
-// the method returns the media card as a DOM div
-// return this.wrapper;
-
-// get mediaSample() {
-    //     return this.mediaSample;
-    // }
-    
-    // handleLikesButton() {
-        //     const that = this;
-        
-        //     this.wrapper
-        //         .querySelector('.likes-btn')
-        //         .addEventListener('click', function() {
-            //             if (this.classList.contains('liked')) {
-                //                 this.classList.remove('liked')
-                //                 that.likesCount.fire('DEC')
-                //             } else {
-                    //                 this.classList.add('liked')
-                    //                 that.likesCount.fire('INC')
-                    //             }
-                    //         })
-                    // }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,28 +4,29 @@
 //     -------------------------------------------------------------------------------------------
 
 
-// collect the ID attached to the selected photographer
+// ... collect the ID attached to the selected photographer
 const searchParams = new URLSearchParams(window.location.search);
 const photographerIdUrl = Number(searchParams.get("id"));
 
+
 async function displayData() {
 
-    // call the fetch function & destructure the photographers & medias
+    // ... call fetch function & destructure the photographers & medias
     const { photographers, media } = await getPhotographers();
-    
-    // collect the selected photographer and his/her works through filter on ID & destructure it
+
+    // ... collect the selected photographer and his/her works through filter on ID & destructure it
     const [selectedPhotographer] = photographers.filter(photographer => photographer.id === photographerIdUrl);
 
     const mediaList = media.filter(media => media.photographerId === photographerIdUrl);
 
-    // calling the photographerFactory (and its getCardDOM method) for the selected photographer
+    // ... calling the photographerFactory (and its getCardDOM method) for the selected photographer
     const photographerModel = photographerFactory(selectedPhotographer);
     const CardDOM = photographerModel.getCardDOM();
-    
-    // display card inside the DOM "photograph-header" for selected photographer
+
+    // ... display card inside the DOM "photograph-header" for selected photographer
     const photographerSection = document.querySelector(".photograph-header");
     photographerSection.appendChild(CardDOM);
-    
+
     const link = document.querySelector("article > a");
     link.remove();
 
@@ -33,27 +34,57 @@ async function displayData() {
     img.setAttribute("src", `assets/photographers/Photographers ID Photos/${selectedPhotographer.portrait}`);
     img.setAttribute("alt", " ");
     img.setAttribute("aria-label", selectedPhotographer.name);
-    
+
     const h2 = document.createElement( "h2" );
     h2.textContent = selectedPhotographer.name;
-    
-    const article = document.querySelector("article");
-    article.appendChild(img);
-    article.appendChild(h2);  
 
-    // display media cards inside the DOM "medias-wrapper"
+    const pPrice = document.querySelector( "p.prix" );
+    console.log(pPrice);
+
+    const article = document.querySelector( "article" );
+    article.appendChild(img);
+    article.appendChild(h2);
+
+    // ... display media cards inside the DOM "medias-wrapper"
     const mediaSection = document.querySelector(".medias-wrapper");
-    
 
     mediaList.forEach(media => {
         const template = new MediaCard(selectedPhotographer, media).createMediaCard();
-    
         mediaSection.innerHTML += template;
     });
-    
+
+	function totalLikes() {
+        // const totalLikesView = document.createElement("span");
+		let nbLikes = 0;
+		mediaList.map((media) => {
+			nbLikes = nbLikes + media.likes;
+		});
+		console.log(nbLikes);
+		pPrice.insertAdjacentHTML("afterbegin", `<span id="totalLikes">${nbLikes} </span>`);
+        pPrice.insertAdjacentHTML("afterbegin","<img id='totalLikes' src='assets/icons/heart.svg'/>");
+    }
+
+    totalLikes();
+	// handleLikesButton() {
+
+	//         this.querySelector('.likes-btn')
+	//             .addEventListener('click', function() {
+	//                     if (this.classList.contains('liked')) {
+	//                             this.classList.remove('liked')
+	//                             that.likesCount.fire('DEC')
+	//                         } else {
+	//                                 this.classList.add('liked')
+	//                                 that.likesCount.fire('INC')
+	//                             }
+	//                         })
+	//                 };
+
+	// handleLikesButton()
+
 };
 
 displayData();
+
 
 // const tab = [1,2,3,4,5,7,8,9];
 
