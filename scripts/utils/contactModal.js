3b67ab2b909008be66eacaf198a5c4f3ac33a8d5
Elodie Civera <elodie.civera5@gmail.com>
2022-05-27 12:29:11 +0200

    wip on contact modal

--- scripts/utils/contactModal.js
+++ scripts/utils/contactModal.js
@@ -2,136 +2,119 @@
 //      Script to display the Contact Modal
 //      -------------------------------------------------------------------------------------------
 
-//  Display Modal is called through onclick on the contact button (see photographer.html)
+const modal = document.querySelector("#contact_modal");
+const modalBtn = document.querySelector(".photograph-header .contact_button");
+const form = document.querySelectorAll("form div");
+const close = document.querySelector(".close");
+const userInputs = document.querySelectorAll("input");
+const submit = document.querySelector(".submit");
 
+const regChar = /^[A-Za-z]{2,25}$/;
+const regEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/;
+
+modalBtn.addEventListener("click", displayModal);
+close.addEventListener("click", closeModal);
+userInputs.forEach(input => input.addEventListener('keyup', checkInput));
+submit.addEventListener("click", checkInput);
+
+//  Display Modal is called through...
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
-
     const whichPhotographer = document.querySelector(".photographer-contact");
     const id = document.querySelector(".photograph-header article > h2");
     whichPhotographer.textContent = id.textContent;
     
-}
+    modal.style.display = "block";
+};
 
 
 //  Close Modal is called through onclick on the "close.svg" (see photographer.html)
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
 
 
-// // FORM validation
-// // -----------------------------------------------------
-// // when form is sumitted (or while inputs are filled in), each user's input is checked & customized error message eventually pop-up
+// FORM validation
+// -----------------------------------------------------
+// when form is sumitted (or while inputs are filled in), each user's input is checked & customized error message eventually pop-up
 
+// value of the "data-error" attribute is the error message, the error class prevents the final validation
 
-// const modalBtn = document.querySelector(".photograph-header .contact_button");
-// const form = document.querySelectorAll("form div");
-// const close = document.querySelector("img");
-// const userInputs = document.querySelectorAll("input");
-// const submit = document.querySelector("form div > .contact-button");
+function setErrorMessage(input, message) {
+	input.parentElement.setAttribute("data-error", message);
+	input.parentElement.setAttribute("data-error-visible", "true");
+	input.parentElement.classList.add("error");
+};
 
-// const regChar = /^[A-Za-z]{2,25}$/;
-// const regEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/;
+// no message when everything is fine, remove the "error" class in case it has been corrected afterwards
 
-// submit.addEventListener("click", checkInput);
+function setSuccessMessage(input) {
+	input.parentElement.removeAttribute("data-error");
+	input.parentElement.removeAttribute("data-error-visible");
+	input.parentElement.classList.remove("error");
+};
 
-// userInputs.forEach(input => input.addEventListener('keyup', checkInput));
+// prevent submitting while inputs are validated
 
-// // // value of the "data-error" attribute is the error message, the error class prevents the final validation
+function checkInput(e) {
+	e.preventDefault();
 
-// function setErrorMessage(input, message) {
-//   input.parentElement.setAttribute("data-error", message);
-//   input.parentElement.setAttribute("data-error-visible", "true");
-//   input.parentElement.classList.add("error");
-// }
+	// each input is switched as per its name and then its value is checked
 
-// // // no message when everything is fine, remove the "error" class in case it has been corrected afterwards
+	userInputs.forEach((input) => {
+		let inputName = input.name;
+		let inputValue = input.value;
 
-// function setSuccessMessage(input) {
-//   input.parentElement.removeAttribute("data-error");
-//   input.parentElement.removeAttribute("data-error-visible");
-//   input.parentElement.classList.remove("error"); 
-// }
+		// if value does not match validation requirement then call function setErrorMessage
+		// else call setSuccessMessage
 
-// // // prevent submitting while inputs are validated
+		switch (inputName) {
+			case "first":
+				if (!regChar.test(inputValue)) {
+					setErrorMessage(input, "Veuillez entrer au moins 2 lettres");
+				} else {
+					setSuccessMessage(input);
+				}
+				break;
 
-// function checkInput(e) {
-//   e.preventDefault();
+			case "last":
+				if (!regChar.test(inputValue)) {
+					setErrorMessage(input, "Veuillez entrer au moins 2 lettres");
+				} else {
+					setSuccessMessage(input);
+				}
+				break;
 
-// // // each input is switched as per its name and then its value is checked
+			case "email":
+				if (!regEmail.test(inputValue)) {
+					setErrorMessage(input, "Veuillez vérifier votre adresse e-mail");
+				} else {
+					setSuccessMessage(input);
+				}
+				break;
 
-//   userInputs.forEach(input => {
-//     let inputName = input.name;
-//     let inputValue = input.value;
-  
-// // // if value does not match validation requirement then call function setErrorMessage
-// // // else call setSuccessMessage
+			case "message":
+				if (!regChar.test(inputValue)) {
+					setErrorMessage(input, "Veuillez entrer au moins 2 lettres");
+				} else {
+					setSuccessMessage(input);
+				}
+				break;
+		}
 
-//     switch (inputName) {
+		// // final loop checking no uncorrected error remaining and final form validation
 
-//   case "first" :
-//     if (!regChar.test(inputValue)) {
-//       setErrorMessage(input, "Veuillez entrer au moins 2 lettres");
-//     } else {
-//         setSuccessMessage(input);
-//     }
-//     break;
+		let ilYaUneErreur = 0;
+		form.forEach((formData) => {
+			if (formData.classList.contains("error")) {
+				ilYaUneErreur++;
+			}
+		});
 
-//   case "last" :
-//     if (!regChar.test(inputValue)) {
-//       setErrorMessage(input, "Veuillez entrer au moins 2 lettres");
-//     } else {
-//         setSuccessMessage(input);
-//     }
-//     break;
-
-//   case "email" :
-//     if (!regEmail.test(inputValue)) {
-//       setErrorMessage(input, "Veuillez vérifier votre adresse e-mail");
-//     } else {
-//         setSuccessMessage(input);
-//     }
-//     break;
-
-//   case "message" :
-//     if (!regChar.test(inputValue)) {
-//       setErrorMessage(input, "Veuillez entrer au moins 2 lettres");
-//     } else {
-//         setSuccessMessage(input);
-//     }
-//     break;
-
-//   }
-
-// // // final loop checking no uncorrected error remaining and final form validation
-
-// let ilYaUneErreur = 0;
-//   form.forEach(formData => {
-//     if (formData.classList.contains("error")) {
-//       ilYaUneErreur++;
-//     }
-//   });
-
-// if (ilYaUneErreur === 0) {
-
-//   const validation = document.querySelector(".modal");
-//   const finalMessage = document.createElement("div");
-//   const finalButton = document.createElement("button");
-
-//   validation.classList.add("final");
-//   finalMessage.textContent = 'Votre message a bien été envoyé';
-//   finalButton.textContent="Fermer";
-
-//   finalButton.classList.add("btn-signup");
-
-//   validation.appendChild(finalMessage);
-//   validation.appendChild(finalButton);
-
-//   finalButton.addEventListener("click", closeModal);
-
-// };
-// })
-// }
+		if (ilYaUneErreur === 0) {
+            userInputs.forEach((input) => {
+			console.log(input.value)
+        });
+        // closeModal();
+		}
+	});
+}
